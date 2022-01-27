c3434d19a5868256d1f66967ae191b6eea519f4d
zhuravkov <d.zhuravkov@itransition.com>
2022-01-27 18:34:33 +0300

    Add get all post endpoint.

--- Web/src/controllers/posts.ts
+++ Web/src/controllers/posts.ts
@@ -34,12 +34,28 @@ const getPost = async (req: Request, res: Response) => {
   return res.status(200).send(post)
 }
 
+const getAllPosts = async (req: Request, res: Response) => {
+  const { error, data } = await getPostsModel({})
+
+  if (error) {
+    return res.status(400).send(error)
+  }
+
+  if (!data || !data.length) {
+    return res.status(404).send()
+  }
+
+  return res.status(200).send(data)
+}
+
 export {
   createPost,
-  getPost
+  getPost,
+  getAllPosts
 }
 
 export default {
   createPost,
-  getPost
+  getPost,
+  getAllPosts
 }
--- Web/src/routes/posts.ts
+++ Web/src/routes/posts.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createPost, getPost } from '../controllers/posts'
+import { createPost, getPost, getAllPosts } from '../controllers/posts'
 import { auth } from '../middleware/auth'
 
 const router = express.Router()
 
 router.post('/posts/create', auth, createPost)
 router.get('/posts/:id', auth, getPost)
+router.get('/posts', auth, getAllPosts)
 
 export default router
