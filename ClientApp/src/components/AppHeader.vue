85c5276b7500aebd69e22510656ad701eb764633
Dmitry Zhuravkov <d.zhuravkov@itransition.com>
2022-03-22 15:38:14 +0400

    Add locale change.

--- ClientApp/src/components/AppHeader.vue
+++ ClientApp/src/components/AppHeader.vue
@@ -6,27 +6,43 @@
     >
       The Light
     </router-link>
-    <NButton
-      v-if="isLoggedUser"
-      text
-      color="white"
-      @click="logOut"
-    >
-      {{ t('log_out') }}
-    </NButton>
+    <div class="app-header__actions">
+      <NDropdown
+        :options="localeOptions"
+        trigger="click"
+        show-arrow
+        @select="onSelectLocale"
+      >
+        <NButton
+          text
+          color="white"
+        >
+          {{ selectedLocale }}
+        </NButton>
+      </NDropdown>
+      <NButton
+        v-if="isLoggedUser"
+        text
+        color="white"
+        @click="logOut"
+      >
+        {{ t('log_out') }}
+      </NButton>
+    </div>
   </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { routerHelper } from '@/utils'
-import { NButton } from 'naive-ui'
+import { defineComponent, computed } from 'vue'
+import { routerHelper, enums } from '@/utils'
+import { NButton, NDropdown } from 'naive-ui'
 import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'AppHeader',
   components: {
-    NButton
+    NButton,
+    NDropdown
   },
   props: {
     isLoggedUser: {
@@ -38,17 +54,41 @@ export default defineComponent({
     logout: (e: Event) => e
   },
   setup (props, vm) {
-    const { t } = useI18n()
+    const { Locales } = enums
+    const { t, locale } = useI18n()
+    const localeOptions = [
+      {
+        label: 'English',
+        key: Locales.EN
+      },
+      {
+        label: 'Русский',
+        key: Locales.RU
+      }
+    ]
+
+    const selectedLocale = computed(() => {
+      return locale.value.toUpperCase()
+    })
+
+    const onSelectLocale = (key: string) => {
+      if (locale.value !== key) {
+        locale.value = key
+      }
+    }
 
     const logOut = (e: Event) => {
       vm.emit('logout', e)
     }
 
     return {
+      t,
+      selectedLocale,
+      localeOptions,
       toMainPageRoute: {
         name: routerHelper.names.Home
       },
-      t,
+      onSelectLocale,
       logOut
     }
   }
@@ -76,6 +116,12 @@ export default defineComponent({
     font-weight: $font-weight-medium;
     font-size: 22px;
     text-decoration: none;
+  }
+
+  &__actions {
+    display: flex;
+    align-items: center;
+    gap: 10px;
   }
 }
 </style>
